2203a7f01eb944ac6d24efeb26f36321d899c384
Hugo Martinez <hugommarting@gmail.com>
2022-02-09 10:41:33 -0500

    test serach screen

--- src/test/routers/DashboardRoutes.test.js
+++ src/test/routers/DashboardRoutes.test.js
@@ -9,7 +9,7 @@ import { MemoryRouter } from "react-router-dom";
 
 describe('test in <DashboardRoutes />', () => {
   
-    test('should render correctli', () => {
+    test('should render correctli in marvel screen', () => {
         
         const contextValue = {
             user: {
@@ -20,7 +20,7 @@ describe('test in <DashboardRoutes />', () => {
 
         const wrapper = mount(
             <AuthContext.Provider value={ contextValue }>
-                <MemoryRouter>
+                <MemoryRouter initialEntries={ ['/'] }>
                     <DashboardRoutes />
                 </MemoryRouter>
             </AuthContext.Provider>
@@ -29,9 +29,33 @@ describe('test in <DashboardRoutes />', () => {
         
         expect( wrapper ).toMatchSnapshot();
         expect( wrapper.find('.text-info').text().trim() ).toBe('User logged');
+        expect( wrapper.find('h1').text().trim() ).toBe('Marvel Screen');
 
 
     });
     
+    test('should render correctli in dc screen', () => {
+        
+        const contextValue = {
+            user: {
+                logged: true,
+                name: 'User logged'
+            }
+        }
+
+        const wrapper = mount(
+            <AuthContext.Provider value={ contextValue }>
+                <MemoryRouter initialEntries={ ['/dc'] }>
+                    <DashboardRoutes />
+                </MemoryRouter>
+            </AuthContext.Provider>
+        )
+
+        
+        expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find('h1').text().trim() ).toBe('Dc Screen');
+
+
+    });
 
 });
