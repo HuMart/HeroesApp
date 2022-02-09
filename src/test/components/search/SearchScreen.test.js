5857b28df132ee9513fc331f9f128c05dbecbd4f
Hugo Martinez <hugommarting@gmail.com>
2022-02-09 15:22:17 -0500

    test serach screen

--- src/test/components/search/SearchScreen.test.js
+++ src/test/components/search/SearchScreen.test.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { MemoryRouter } from "react-router-dom";
 
 import { mount } from "enzyme";
 import { SearchScreen } from "../../../components/search/SearchScreen";
+
+
+describe('testing <SearchScreen />', () => {
+  
+    test('should render correctly with values by default', () => {
+      
+        const wrapper = mount(
+        <MemoryRouter initialEntries={ ['/search'] }>
+            <SearchScreen />
+        </MemoryRouter>
+      
+        )
+        expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find('h1').text().trim() ).toBe('Search');
+
+    });
+    
+
+});
+
 
